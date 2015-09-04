d9f84f22f01e195310df02a699d7fa2727a29b7b
hoonsgood <hoonsgood@gmail.com>
2015-09-05 00:23:13 +0900

    add vimrc.local copy

--- build_environment.sh
+++ build_environment.sh
@@ -19,5 +19,6 @@ echo "Cloning Vundle.vim from github"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 sudo npm install jshint -g
+cp vim/vimrc.local ~/.vimrc.local
 
 echo "Done!"
